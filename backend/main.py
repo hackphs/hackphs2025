f89de33e26971bfedf4f4e4c2862b6a9be13e0a4
Om Mehta <ommehta16@icloud.com>
2025-06-03 18:31:28 -0400

    polish up frontend, try to figure out CORS

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@
 
 import json, os, markupsafe
 from flask import Flask, request, make_response
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
@@ -10,15 +11,21 @@ emails = set()
 if not os.path.exists("emails.json"):
     with open ("emails.json",'w') as f: f.write("[]")
 
-@app.route("/",methods=["POST"])
+@app.route("/",methods=["GET"])
+@cross_origin()
 def add_registration():
     global emails
     try:
-        email = str(json.loads(request.data.decode())["email"])
+        email = request.args["email"]
 
-        if email in emails: return "L", 200 # L --> not added
+        if email in emails:
+            return "L", 200 # L --> not added
         with open ("emails.json",'r') as f: emails = set(json.load(f))
         emails.add(email)
         with open ("emails.json",'w') as f: json.dump(list(emails),f)
-    except: return "L", 500
-    return "W", 200 # W --> added
+    except:
+        return "LA", 200
+    return "W",200 # W --> added
+
+if __name__ == "__main__":
+    app.run("127.0.0.1",8000)
