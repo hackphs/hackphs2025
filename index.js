f03b3580854c82e6f39cc6bbf4fa22c09bba115c
Om Mehta <ommehta16@icloud.com>
2025-06-04 16:47:15 -0400

    Make site validate empty strings

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ const emailInp = document.querySelector("input");
 
 button.addEventListener("click", async e => {
     e.preventDefault();
-
-    if (!form.reportValidity()) return;
+    emailInp.checkValidity();
+    if (!form.checkValidity() || emailInp.value == "") {
+        if (emailInp.value == "") emailInp.setCustomValidity("Must enter an email");
+        else emailInp.setCustomValidity("");
+        emailInp.reportValidity();
+        form.reportValidity();
+        return;
+    }
 
     // NOW SEND THE POST REQUEST
     const response = await fetch("https://hackphs.pythonanywhere.com/", {
         method: "POST",
         body: JSON.stringify({ email: emailInp.value }),
     });
+
+    button.className = "notified";
+
+    setTimeout(() => {
+        emailInp.addEventListener("change", () => { button.className = "unnotified"; });
+    }, 500);
+
     if (emailInp.value == "storby@hackphs.tech") {
         document.body.className = "sunset";
         setTimeout(() => document.body.className = "night", 14500);
     }
-
-    button.className = "notified";
-});
-
-// om admits orz
-
-const lastPressed = [];
-
-window.addEventListener("keydown", e => {
-    lastPressed.push(e.key.toLowerCase());
-    // if (lastPressed.length < 5) return;
-
-    document.querySelector("#birdy").style.setProperty("--rep", "1");
 });
