427e73350cd53759a80e1171904074cbf1a1e575
Jonathan Ji <0jonathanji0@gmail.com>
2025-06-04 13:54:30 -0400

    squawk

--- index.js
+++ index.js
@@ -24,3 +24,19 @@ button.addEventListener("click", async e => {
 });
 
 // om admits orz
+
+const lastPressed = []; // change
+const thing = []; // no change
+
+
+
+window.addEventListener("keydown", e => {
+    lastPressed.push(e.key);
+    if (lastPressed.length<5) {
+        return;
+    }
+    let n = lastPressed.length;
+    if (lastPressed[n-1]=="c" && lastPressed[n-2]=="x" && ((lastPressed[n-3]=="ArrowDown" && lastPressed[n-4]=="ArrowRight") || (lastPressed[n-3]=="ArrowRight" && lastPressed[n-4]=="ArrowDown")) && lastPressed[n-5]=="c") {
+        // BIRD!!
+    }
+});
