33f4bdfbbb5d69ae3a0246dd947a4873e754dc27
Om Mehta <ommehta16@icloud.com>
2025-06-04 12:18:34 -0400

    Add epic easter eggs

--- index.js
+++ index.js
@@ -7,20 +7,28 @@ const emailInp = document.querySelector("input");
 button.addEventListener("click", async e => {
     e.preventDefault();
 
-    if (!form.reportValidity()) {
-        return;
-    }
+    if (!form.reportValidity()) return;
 
     // NOW SEND THE POST REQUEST
-    // OM SO ORZ
     const response = await fetch("https://hackphs.pythonanywhere.com/", {
         method: "POST",
-        
         body: JSON.stringify({ email: emailInp.value }),
     });
-    console.log(emailInp.value);
-    console.log(response.text());
+    if (emailInp.value == "storby@hackphs.tech") {
+        document.body.className = "sunset";
+        setTimeout(() => document.body.className = "night", 14500);
+    }
+
     button.className = "notified";
 });
 
 // om admits orz
+
+const lastPressed = [];
+
+window.addEventListener("keydown", e => {
+    lastPressed.push(e.key.toLowerCase());
+    // if (lastPressed.length < 5) return;
+
+    document.querySelector("#birdy").style.setProperty("--rep", "1");
+});
