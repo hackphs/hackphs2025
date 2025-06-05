ac0ca3178dbedfd2acc06eacac883ad3ad38faf1
Om Mehta <ommehta16@icloud.com>
2025-06-05 11:31:31 -0400

    make button work

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const button = document.querySelector("button");
 const form = document.querySelector("form");
 const emailInp = document.querySelector("input");
 
-button.addEventListener("click", async e => {
+button.addEventListener("click", e => {
     e.preventDefault();
+    emailInp.setCustomValidity("");
     emailInp.checkValidity();
     if (!form.checkValidity() || emailInp.value == "") {
         if (emailInp.value == "") emailInp.setCustomValidity("Must enter an email");
@@ -16,23 +17,17 @@ button.addEventListener("click", async e => {
     }
 
     // NOW SEND THE POST REQUEST
-    const response = await fetch("https://hackphs.pythonanywhere.com/", {
+    fetch("https://hackphs.pythonanywhere.com/", {
         method: "POST",
         body: JSON.stringify({ email: emailInp.value }),
-    });
+    }).then(data => console.log(data.text()));
 
     button.className = "notified";
-
-    setTimeout(() => {
-        emailInp.addEventListener("change", () => { button.className = "unnotified"; });
-    }, 500);
 
     if (emailInp.value == "storby@hackphs.tech") {
         document.body.className = "sunset";
         setTimeout(() => document.body.className = "night", 14500);
     }
-
-    button.className = "notified";
 });
 
 // om admits orz
