c678379a6ca792fefb1aa280b9fec9d087615686
Om Mehta <ommehta16@icloud.com>
2025-06-05 10:19:04 -0400

    bird is like that

--- index.js
+++ index.js
@@ -40,15 +40,13 @@ button.addEventListener("click", async e => {
 const lastPressed = []; // change
 const thing = []; // no change
 
-
-
 window.addEventListener("keydown", e => {
     lastPressed.push(e.key);
-    if (lastPressed.length<5) {
+    if (lastPressed.length < 5) {
         return;
     }
     let n = lastPressed.length;
-    if (lastPressed[n-1]=="c" && lastPressed[n-2]=="x" && ((lastPressed[n-3]=="ArrowDown" && lastPressed[n-4]=="ArrowRight") || (lastPressed[n-3]=="ArrowRight" && lastPressed[n-4]=="ArrowDown")) && lastPressed[n-5]=="c") {
+    if (lastPressed[n - 1] == "c" && lastPressed[n - 2] == "x" && ((lastPressed[n - 3] == "ArrowDown" && lastPressed[n - 4] == "ArrowRight") || (lastPressed[n - 3] == "ArrowRight" && lastPressed[n - 4] == "ArrowDown")) && lastPressed[n - 5] == "c") {
         // BIRD!!
     }
 })
