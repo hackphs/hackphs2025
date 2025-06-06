d44c3a83bf9a0b4e06d7490012bd26905f5d2f0c
Om Mehta <ommehta16@icloud.com>
2025-06-06 10:31:30 -0400

    more esster aggs

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ const birdFly = [
 ];
 
 let flown = false;
+
+const allEqual = (a, b) => {
+    if (a.length != b.length) return false;
+    for (let i = 0; i < a.length; i++) if (a.at(i) != b.at(i)) return false;
+    return true;
+}
+
 window.addEventListener("keydown", e => {
     lastPressed.push(e.key);
     console.log(e);
@@ -68,7 +75,6 @@ window.addEventListener("keydown", e => {
     const birdy = document.querySelector("#birdy");
 
     if (lastPressed[n - 1] == "c" && lastPressed[n - 2] == "x" && ((lastPressed[n - 3] == "ArrowDown" && lastPressed[n - 4] == "ArrowRight") || (lastPressed[n - 3] == "ArrowRight" && lastPressed[n - 4] == "ArrowDown")) && lastPressed[n - 5] == "c") {
-        console.log("fdashkjfaskj");
         flown = true;
         birdy.style.display = "block";
         birdy.animate(birdFly, {
@@ -77,5 +83,15 @@ window.addEventListener("keydown", e => {
             fill: "forwards"
         });
         sleep(2000).then(() => { document.querySelector("h1").style.setProperty("color", "rgba(62,151,192,255)"); });
+    }
+    if (n < 10) return;
+
+    console.log(lastPressed.slice(n - 10));
+    if (allEqual(lastPressed.slice(n - 10), ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"])) {
+        document.body.style.overflow = "hidden";
+        document.querySelectorAll("body *").forEach(el => el.animate([
+            { transform: "rotate(0deg)" },
+            { transform: "rotate(360deg)" }
+        ], { duration: 2000 }));
     }
 });
